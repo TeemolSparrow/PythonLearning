c59c8a680242bbccc4e875e032523af9fd64916b
TeemolSparrow <mtian@incarcloud.com>
2019-02-13 18:09:00 +0800

    增加导弹相关设置

--- alien_invasion/src/main/setting.py
+++ alien_invasion/src/main/setting.py
@@ -19,6 +19,10 @@ class Settings:
         self.bullet_color = (60, 60, 60)
         self.bullet_allowed = 5
 
+        # 导弹设置
+        self.missile_speed = 1
+        self.missile_allowed = 3
+
         # UFO设置
         self.ufo_speed_x = 1
         self.ufo_speed_up_x = 1
